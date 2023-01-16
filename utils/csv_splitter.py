81c39c0d905c63f48fe73b3eba46b8ebd689efef
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2023-01-16 10:53:05 +0800

    Added csv_splitter module to split big csv files

---
+++ utils/csv_splitter.py
@@ -0,0 +1,45 @@
+import os
+
+def split(filehandler, delimiter=',', row_limit=106, 
+    output_name_template='co2_%s.csv', output_path='data', keep_headers=False):
+    """
+    Splits a CSV file into multiple pieces.
+    
+    Arguments:
+        row_limit: The number of rows you want in each output file. 10,000 by default.
+        output_name_template: A %s-style template for the numbered output files.
+        output_path: Where to stick the output files.
+        keep_headers: Whether or not to print the headers in each output file.
+    
+    Example usage:
+        >> from toolbox import csv_splitter;
+        >> csv_splitter.split(open('data/big_dataset.csv', 'r'));
+    
+    """
+    import csv
+    reader = csv.reader(filehandler, delimiter=delimiter)
+    current_piece = 1
+    current_out_path = os.path.join(
+         output_path,
+         output_name_template  % current_piece
+    )
+    current_out_writer = csv.writer(open(current_out_path, 'w', newline=''), delimiter=delimiter)
+    current_limit = row_limit
+    if keep_headers:
+        headers = reader.next()
+        current_out_writer.writerow(headers)
+    for i, row in enumerate(reader):
+        if i + 1 > current_limit:
+            current_piece += 1
+            current_limit = row_limit * current_piece
+            current_out_path = os.path.join(
+               output_path,
+               output_name_template  % current_piece
+            )
+            current_out_writer = csv.writer(open(current_out_path, 'w', newline=''), delimiter=delimiter)
+            if keep_headers:
+                current_out_writer.writerow(headers)
+        current_out_writer.writerow(row)
+
+if __name__ == '__main__':
+    split(open('unused/dataset_co2.csv', 'r'))
